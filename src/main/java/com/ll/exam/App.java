c7bb41bd810708f7d6a5f2ae3a58a649169a0aab
Dot <akdlwkd2@gmail.com>
2022-07-07 16:06:51 +0900

    WiseSay구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -8,8 +8,11 @@ public class App {
 
             Scanner sc = new Scanner(System.in);
 
+            int wiseSayingLastId = 0 ; //스코프 : run 함수가 끝나기 전까지 존재
+
             outer:
             while (true) {
+
                 System.out.printf("명령) ");
                 String cmd = sc.nextLine().trim();
 
@@ -19,14 +22,13 @@ public class App {
                         String content = sc.nextLine().trim();
                         System.out.printf("작가 : ");
                         String author = sc.nextLine().trim();
-                        System.out.printf("1번 명언이 등록되었습니다.");
+                        int id = ++wiseSayingLastId; //명언 글 번호 증가
+
+                        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                        System.out.println(wiseSaying);
+
+                        System.out.printf("%d번 명언이 등록되었습니다.\n",id);
                         break;
-
-
-                    case "목록":
-                        System.out.println("번호 / 작가 / 명언");
-                        System.out.println("----------------------");
-
                     case "종료":
                         break outer;
                 }
