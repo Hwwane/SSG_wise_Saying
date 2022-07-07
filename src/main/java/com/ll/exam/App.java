d9cc6dcd923bdc257b60849ecdbd6fe11311a786
Dot <akdlwkd2@gmail.com>
2022-07-07 15:41:31 +0900

    등록기능시행

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -14,11 +14,23 @@ public class App {
                 String cmd = sc.nextLine().trim();
 
                 switch (cmd) {
+                    case "등록":
+                        System.out.printf("명언 : ");
+                        String content = sc.nextLine().trim();
+                        System.out.printf("작가 : ");
+                        String author = sc.nextLine().trim();
+                        System.out.printf("1번 명언이 등록되었습니다.");
+                        break;
+
+
+                    case "목록":
+                        System.out.println("번호 / 작가 / 명언");
+                        System.out.println("----------------------");
+
                     case "종료":
                         break outer;
                 }
             }
-
             sc.close();
         }
     }
