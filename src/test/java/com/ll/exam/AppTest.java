189db8b681e39ca2c34053790c27995907ef8edb
Dot <akdlwkd2@gmail.com>
2022-07-08 14:15:47 +0900

    미리 받을 수 있는 스캐너

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -3,6 +3,10 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
@@ -12,4 +16,24 @@ public class AppTest {
         int rs = 10 + 20;
         assertEquals(30, rs);
     }
+
+    @Test
+    public void 테스트_스캐너(){
+        String input = """
+                등록
+                명언1
+                작가1
+                """.stripIndent(); //앞을 없애줌??
+        InputStream in = new ByteArrayInputStream(input.getBytes()); //스캐너 부분 다시 설명...
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("등록", cmd);
+        assertEquals("명언1", content);
+        assertEquals("작가1", author);
+    }
+
 }
