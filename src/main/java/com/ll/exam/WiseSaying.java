ca8a2ac8a7c4ec0e816212de1daf9cabc5cf9616
Dot <akdlwkd2@gmail.com>
2022-07-08 14:49:07 +0900

    입력받은 명언을 리스트에 저장, 목록기능 구현

--- src/main/java/com/ll/exam/WiseSaying.java
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -1,9 +1,9 @@
 package com.ll.exam;
 
 public class WiseSaying {
-    private int id;
-    private String content;
-    private String author;
+    int id;
+    String content;
+    String author;
 
     @Override
     public String toString() {
